0040e680b4c66063225b2ce3a1b466f683acb57f
Sudhir Kumar Yadav <145853393+YKSudhir@users.noreply.github.com>
2024-10-22 18:27:20 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// // // let n = 5;
+// // // for(let i=1 ; i<=n ; i++)
+// // //     {
+// // //         console.log("Hello, " ,i);
+// // //     }
+// // // console.log("bye");
+// // console.log(process.argv);
+// let args = process.argv;
+// for(let i=2 ; i<args.length ; i++)
+//     {
+//         console.log("hello to " , args[i]);
+//     }
+const info = require("./fruits");
+
+console.log(info);
